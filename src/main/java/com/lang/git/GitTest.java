f86fff896de446c1ee067210ac5b6da239e89d67
jack <jack@qq.com>
2021-11-05 11:31:07 +0800

    second commit

--- src/main/java/com/lang/git/GitTest.java
+++ src/main/java/com/lang/git/GitTest.java
@@ -3,5 +3,6 @@ package com.lang.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
